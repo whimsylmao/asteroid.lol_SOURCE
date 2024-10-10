2aae033b5c1a6f597cdccc778957753c324aaee0
whimsylmao <asteroidbusiness1@gmail.com>
2024-10-10 18:29:31 -0400

    Create READ_ME.lua
    read it fatass

---
+++ READ_ME.lua
@@ -0,0 +1,42 @@
+-- yo!
+
+-- thanks for using asteroid.lol!
+-- this is a roblox script hub made for FUN, if this becomes detected at any time, i most likely wont fix it (unless i get bored again)
+-- leave suggestions in my dms, discord is "asteroidlordfr"
+
+-- thanks for ur time !
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+-- by the way, its obfuscated so people don't try to steal our code and stuff, its basic code but at the same time we want it to be more secure, i'm likely gonna release a unobfuscated version at one point
+
+-- if you don't trust it or think its a rat, simply don't use it
